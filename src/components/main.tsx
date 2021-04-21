b0fef1d5264de5847b41b756fe5c278b88a93214
aman tiwari <amantiwari78632@gmail.com>
2021-04-22 00:00:00 +0530

    reforcing the styling

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -2,9 +2,9 @@ import React, { useContext } from 'react';
 import { Card } from './Card/Card';
 import { Input } from "./Input/Input"
 import { ThemeContext } from "../App";
-import { Col, Row } from "react-bootstrap";
-import styled from 'styled-components'; 
-import formatBytes  from './utils/formatBytes'
+import styled from 'styled-components';
+import formatBytes from './utils/formatBytes'
+import tw from 'twin.macro';
 
 
 const DownloadAll = styled.button`
@@ -95,86 +95,73 @@ const Main = () => {
         })
     }
     return (
-        <Row>
-            <Col xs={12} >
-                <Input />
-            </Col>
-            <Col xs={12}>
+        <div tw="flex flex-col">
+            <Input />
 
-                <Row>
-                    <br />
-                    <Col xs={12}>
-                        {
-                            state.isError.isError && <p>{state.isError.text}</p>
-                        }
-                    </Col>
-
-                    <Col xs={12}>
-                        {
-                            state.PlayListLoading && <p>Please wait.. because your link are playlist. it maybe longer time</p>
-                        }
-                    </Col>
-
-                    <Col xs={12}>
-                        {
-                            state.AllListOfQuaility.length > 0 &&
-                            <Row>
-
-                                <Col xs={3}>
-                                    <label>Overall FileSize : </label>
-                                </Col>
-
-                                <Col xs={2}>
-
-                                    <p>{formatBytes(state.ChangeQuality.totalfilesize[state.ChangeQuality.quality])}</p>
-                                </Col>
-
-                                <Col xs={3}>
-
-                                    <label>Overall Quality : </label>
-                                </Col>
-
-                                <Col xs={2}>
-
-                                    <select value={state.ChangeQuality.quality} onChange={e => HandnleQuality(e.target.value)}>
-                                        {
-                                            state.AllListOfQuaility.map((quality: string) => (
-                                                <option key={quality} >{quality}</option>
-                                            ))
-                                        }
-                                    </select>
-                                </Col>
-                                <Col xs={2}>
-                                    <DownloadAll
-                                        disabled={AllDetail.length === 0}
-                                        type="button"
-                                        onClick={() => All_Download_Video(state.ChangeQuality.quality)}>
-                                        Download
-                                        </DownloadAll>
-
-                                </Col>
-
-                            </Row>
-                        }
-                    </Col>
-                </Row>
-            </Col>
-
-            <Col xs={12}>
-                <>
+            <div>
+                <br />
+                <div>
                     {
-                        AllDetail.map((data: any) => (
-                            <Card key={data.title} UrlExist={state.UrlExist} handleRemoveItem={SetAllDetail} path={state.Path} data={data} />
-                        ))
+                        state.isError.isError && <p>{state.isError.text}</p>
                     }
+                </div>
+
+                <div>
                     {
-                        state.CardLoading && <h1>*******Loading******</h1>
+                        state.PlayListLoading && <p>Please wait.. because your link are playlist. it maybe longer time</p>
                     }
-                </>
-            </Col>
+                </div>
+
+                <div>
+                    {
+                        state.AllListOfQuaility.length > 0 &&
+                        <div tw="grid grid-cols-1 sm:(grid-cols-6 space-x-5) md:grid-cols-5 justify-center items-center ">
 
 
-        </Row>
+                            <div tw="flex col-span-5 sm:col-span-3 md:col-span-2 space-x-5 justify-center" >
+                                <label tw="whitespace-nowrap" >Overall FileSize : </label>
+                                <p tw="whitespace-nowrap" >{formatBytes(state.ChangeQuality.totalfilesize[state.ChangeQuality.quality])}</p>
+                            </div>
+
+
+                            <div tw="flex col-span-5 sm:(col-span-3 col-start-4 ) md:col-span-2 space-x-5 justify-center" >
+                                <label tw="whitespace-nowrap" >Overall Quality : </label>
+                                <select tw=" h-5 dark:(text-black)" value={state.ChangeQuality.quality} onChange={e => HandnleQuality(e.target.value)}>
+                                    {
+                                        state.AllListOfQuaility.map((quality: string) => (
+                                            <option key={quality} >{quality}</option>
+                                        ))
+                                    }
+                                </select>
+                            </div>
+
+                            <div tw="flex justify-center col-span-5  md:col-span-1" >
+                                <DownloadAll
+                                    tw=" w-24 h-10 "
+                                    disabled={AllDetail.length === 0}
+                                    type="button"
+                                    onClick={() => All_Download_Video(state.ChangeQuality.quality)}>
+                                    Download
+                                    </DownloadAll>
+                            </div>
+                        </div>
+                    }
+                </div>
+            </div>
+
+            <div>
+                {
+                    AllDetail.map((data: any) => (
+                        <Card key={data.title} UrlExist={state.UrlExist} handleRemoveItem={SetAllDetail} path={state.Path} data={data} />
+                    ))
+                }
+                {
+                    state.CardLoading && <h1>*******Loading******</h1>
+                }
+            </div>
+
+
+        </div>
     );
 
 }
