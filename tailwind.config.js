114da1eed91bbcbaf2eecc6fc6152be478836826
aman tiwari <amantiwari78632@gmail.com>
2021-04-21 23:52:37 +0530

    added tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  darkMode: "class",
+  theme: {
+    screens: {
+      xs: "0px",
+      sm: "460px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      colors: {
+        darkmode: "#2C3A47",
+        lightmode: "#ffffff",
+        lightnav: "#3d4451",
+        darknav: "#2a2e37",
+      },
+
+      height: {
+        "10v": "10vh",
+        "20v": "20vh",
+        "30v": "30vh",
+        "40v": "40vh",
+        "50v": "50vh",
+        "60v": "60vh",
+        "70v": "70vh",
+        "80v": "80vh",
+        "90v": "90vh",
+        "100v": "100vh",
+      },
+    },
+  },
+  plugins: [],
+};
