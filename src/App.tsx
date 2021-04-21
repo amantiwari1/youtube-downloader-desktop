1af45093625730458e76f42a20eb40ba8477ff0d
aman tiwari <amantiwari78632@gmail.com>
2021-04-21 23:59:16 +0530

    added tw styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, { useReducer, createContext, useEffect } from 'react';
 import { DetailsReducer, stateReducer } from "./Reducer";
-import { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme, GlobalStyle } from "./components/Themes"
+import { ThemeProvider } from "./themes/themeContext";
+import GlobalStyles from "./themes/globalStyles";
 import Main from './components/main'
 import styled from 'styled-components';
 import { Setting } from './components/Setting/Setting';
 import { AiFillSetting } from 'react-icons/ai';
-import {Container} from 'react-bootstrap'
+import tw from "twin.macro"
 
 
 
@@ -49,35 +49,35 @@ const App = () => {
     PlayListLoading: false,
     CardLoading: false,
     AllListOfQuaility: [],
-    ChangeQuality: { totalfilesize: {}, quality: "" }, 
+    ChangeQuality: { totalfilesize: {}, quality: "" },
     isError: { isError: false, text: "" },
     showModal: false,
-    theme: 'light', 
+    theme: 'light',
     is_not_connected: false,
     UrlExist: []
-  } 
+  }
 
   const [state, dispatch] = useReducer(stateReducer, initialState)
   const [AllDetail, SetAllDetail] = useReducer(DetailsReducer, [])
 
-  
+
   useEffect(() => {
     const localTheme = window.localStorage.getItem("theme")
-    localTheme && dispatch({type: 'setTheme', data: localTheme})
+    localTheme && dispatch({ type: 'setTheme', data: localTheme })
   }, [])
 
   const openModal = () => {
-    dispatch({type: 'showModal'})
+    dispatch({ type: 'showModal' })
   };
 
   useEffect(() => {
     window.eel.Get_All_Details()((data: any) => {
 
-    SetAllDetail({ data: data.data , type: 'entire' })
-    dispatch({type: 'entireUrlExist', data: data.UrlExist})
+      SetAllDetail({ data: data.data, type: 'entire' })
+      dispatch({ type: 'entireUrlExist', data: data.UrlExist })
 
 
-    })  
+    })
   }, [])
 
 
@@ -87,20 +87,20 @@ const App = () => {
   function Set_Download_Percent(data: any) {
     SetAllDetail({ data, type: 'updateDownloadPercent' })
   }
-  
+
   function Set_Savefile(data: any) {
     SetAllDetail({ data, type: 'updateSavefile' })
   }
 
 
   function is_not_connected(bool: boolean) {
-    dispatch({ data: bool , type: 'is_not_connected' })
-    
+    dispatch({ data: bool, type: 'is_not_connected' })
+
   }
   function set_AllDetails(data: any) {
-    SetAllDetail({ data: data.data , type: 'entire' })
-    dispatch({type: 'entireUrlExist', data: data.UrlExist})
-    
+    SetAllDetail({ data: data.data, type: 'entire' })
+    dispatch({ type: 'entireUrlExist', data: data.UrlExist })
+
   }
 
   // this Set_Download_Percent will be sent in python and 
@@ -113,24 +113,20 @@ const App = () => {
 
   return (
 
-    <ThemeProvider theme={state.theme === 'light' ? lightTheme : darkTheme}>
-      <ThemeContext.Provider 
-      value={{ 
-        AllDetail,
-        SetAllDetail,
-        state,
-        dispatch
+    <ThemeProvider>
+      <ThemeContext.Provider
+        value={{
+          AllDetail,
+          SetAllDetail,
+          state,
+          dispatch
         }}>
-      <Container>
-        <Setting  />
-
-
-        <Button onClick={openModal}  />
-      <GlobalStyle />
-
-        <Main />
-      
-      </Container>
+        <div tw="max-w-2xl mx-auto p-4" >
+          <Setting />
+          <GlobalStyles />
+          <Button onClick={openModal} />
+          <Main />
+        </div>
       </ThemeContext.Provider>
     </ThemeProvider>
   );
